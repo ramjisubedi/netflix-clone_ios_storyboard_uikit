57d4613436a7ad3374cdab9dc042319ce3b5b652
ramji subedi <ramjisubedi1991@gmail.com>
2022-08-29 21:48:00 +0545

    set screen for home view (UITabBarController)

--- Netflix Clone/ViewController.swift
+++ Netflix Clone/ViewController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemRed
         // Do any additional setup after loading the view.
     }
 
